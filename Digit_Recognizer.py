c7712bc4cfe37b45503f0cc9a07a3663d4da032e
souravnanda <souravnanda41@gmail.com>
2020-07-21 22:06:18 +0530

    Adding Files

---
+++ Digit_Recognizer.py
@@ -0,0 +1,18 @@
+from tensorflow.keras.models import load_model
+import gradio as gr
+
+model_l=load_model('/home/jovyan/work/digit_model_deepnote.h5')
+
+def digit_classifier(image):
+    image=image.reshape(1,28,28,1)
+    pred=model_l.predict_classes(image)[0]
+    return str(pred)
+
+interface=gr.Interface(fn=digit_classifier,
+                       inputs=gr.inputs.Sketchpad(shape=(28,28)),
+                       outputs='label',
+                       live=True,
+                       capture_session=True,
+                       title='Handwritten Digit Recognizer',
+                       description='Write any number and see the predictions in terms of labels')
+interface.launch()
